96c693002a885cc8d1b3c7ac4b1fb61fcbe86a7c
shreya mumbale <shreyamumbale19@gmail.com>
2026-08-25 23:25:14 +0530

    AverageThreeNumbers

---
+++ AverageThreeNumbers.java
@@ -0,0 +1,9 @@
+class AverageThreeNumbers {
+  public static void main(String[]args){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int average = (a +b+ c)/3;
+    System.out.println(average);
+  }
+}
